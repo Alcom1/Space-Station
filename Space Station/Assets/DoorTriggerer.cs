0e8716f196566d8f7d0e5f24da72ff020e327457
unknown <cxs1466@IMM-4.main.ad.rit.edu>
2015-12-11 10:49:56 -0500

    Animation work begin

---
+++ Space Station/Assets/DoorTriggerer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTriggerer : MonoBehaviour {
+	public GameObject door1;
+	public GameObject door2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		Debug.Log("Trigger");
+		if (col.gameObject.tag == "Door1") {
+			Debug.Log("Door1");
+			if(!door1.GetComponent<Animation>().isPlaying){
+				door1.GetComponent<Animation>().Play();
+			}
+		}
+	}
+}

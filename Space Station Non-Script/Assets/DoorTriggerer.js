a4819766fe9df700ea2d4ebdbeea32118fc110e6
CScavone96 <Cscavone96@gmail.com>
2015-12-17 17:24:10 -0500

    Began animation work and fixed some textures

---
+++ Space Station Non-Script/Assets/DoorTriggerer.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var airlockdoor : GameObject;
+
+function Start () {
+
+}
+
+function OnTriggerEnter (other : Collider) {
+		if(other.gameObject.tag == "AirlockTrigger"){
+				airlockdoor.GetComponent(SmoothMoveJS).Move();
+		}
+	}
+
+function Update () {
+
+}
---
+++ Space Station Non-Script/Assets/SmoothMoveJS.js
@@ -0,0 +1,19 @@
+﻿var startspot : Transform;
+var endspot : Transform;
+ 
+function Start () {
+    //SmoothMove(startspot.position, endspot.position, 5.0);
+}
+
+function Move(){
+	SmoothMove(startspot.position, endspot.position, 5.0);
+}
+ 
+function SmoothMove (startpos : Vector3, endpos : Vector3, seconds : float) {
+    var t = 0.0;
+    while (t <= 1.0) {
+        t += Time.deltaTime/seconds;
+        transform.position = Vector3.Lerp(startpos, endpos, Mathf.SmoothStep(0.0, 1.0, t));
+        yield;
+    }
+}

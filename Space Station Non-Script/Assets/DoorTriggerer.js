7fc1de4aa842b6146f23f3e8d52cded6ec7976d4
CScavone96 <Cscavone96@gmail.com>
2015-12-17 17:34:08 -0500

    Second door animated and third door added to security hallway, can't open. More texture fixes.

--- Space Station Non-Script/Assets/DoorTriggerer.js
+++ Space Station Non-Script/Assets/DoorTriggerer.js
@@ -1,6 +1,7 @@
 ﻿#pragma strict
 
 var airlockdoor : GameObject;
+var securitydoor : GameObject;
 
 function Start () {
 
@@ -9,6 +10,9 @@ function Start () {
 function OnTriggerEnter (other : Collider) {
 		if(other.gameObject.tag == "AirlockTrigger"){
 				airlockdoor.GetComponent(SmoothMoveJS).Move();
+		}
+		if(other.gameObject.tag == "SecurityTrigger"){
+				securitydoor.GetComponent(SmoothMoveJS).Move();
 		}
 	}
 
